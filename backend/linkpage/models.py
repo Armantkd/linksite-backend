66e89c383e23ac30d14cdc646e18f4c294b3904c
Arman <armantkidd@gmail.com>
2023-01-12 12:35:27 -0500

    changed models and security

--- backend/linkpage/models.py
+++ backend/linkpage/models.py
@@ -4,12 +4,16 @@ class Creator(models.Model):
     name = models.CharField(max_length=255)
     views = models.IntegerField()
     replies = models.IntegerField()
+    blink = models.SlugField(max_length=255)
     class Meta:
         ordering = ['-views']
+        db_table = "creator"
 
 
 class Rentry(models.Model):
     creator = models.ForeignKey('Creator', on_delete = models.CASCADE)
     link = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    class Meta:
+        db_table = "rentry"
     
